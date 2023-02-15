e99a3e832f47dac9571784c648c038b99ee9cd39
Deborah Anne Gormley <debi.gormley@gmail.com>
2023-02-15 22:11:46 +0000

    third commit for week 02

---
+++ hello2.py
@@ -0,0 +1,6 @@
+# hello2.py
+#this program allow the user to enter their name to be greated kindly"
+#Author Debi Gormley
+
+name = input ("enter your name:")
+print ("Hello " + name + " nice to meet you")
